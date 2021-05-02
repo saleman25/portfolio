54999c06fa1352a7e750075083659e79c49be5c0
Samantha Aleman <samantha.aleman25@gmail.com>
2021-05-01 20:03:54 -0700

    started working on the about me section

---
+++ src/components/Aboutme.js
@@ -0,0 +1,23 @@
+import React, {Component} from 'react';
+
+
+class Aboutme extends Component {
+    render () {
+        return (
+            <div>
+                <h1>About Me</h1>
+                <p>
+                    Howdy! My name is Samantha. I am a full-stack web developer. 
+                    I'm specialized in Ruby and Javascript. 
+                    I have a passion for frontend developement
+                </p>
+                <br></br>
+                <p>
+                    I also love the space, 80's new wave, horror movies, and corgis especially mine, Anakin.
+                </p>
+            </div>
+        );
+    }
+}
+
+export default Aboutme;
