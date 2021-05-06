a646a8895d2e49babaf3ebcf5ae426b891986747
Samantha Aleman <samantha.aleman25@gmail.com>
2021-05-06 12:56:00 -0700

    redid the layout and finalized the skeleton of projects

---
+++ src/components/Projects.js
@@ -0,0 +1,64 @@
+// add the video demos of yr projects
+// autoplay when hovered over
+
+import React from 'react';
+import '../App.css';
+
+import Grid from '@material-ui/core/Grid';
+
+
+function Projects() {
+  return (
+    <div>
+
+    <div class='projectcard'>
+      <h1>this is the title</h1>
+      <div class='img'>
+        <p>this is going to be the image</p>
+      </div>
+      <div class='description'>
+        <p>this is going to be the description</p>
+      </div>
+      <p>i need to add hyperlinks for the repo and demo</p>
+    </div>
+
+    <div class='projectcard'>
+      <h1>this is the title</h1>
+      <div class='img'>
+        <p>this is going to be the image</p>
+      </div>
+      <div class='description'>
+        <p>this is going to be the description</p>
+      </div>
+      <p>i need to add hyperlinks for the repo and demo</p>
+    </div>
+
+    <div class='projectcard'>
+      <h1>this is the title</h1>
+      <div class='img'>
+        <p>this is going to be the image</p>
+      </div>
+      <div class='description'>
+        <p>this is going to be the description</p>
+      </div>
+      <p>i need to add hyperlinks for the repo and demo</p>
+    </div>
+
+    <div class='projectcard'>
+      <h1>this is the title</h1>
+      <div class='img'>
+        <p>this is going to be the image</p>
+      </div>
+      <div class='description'>
+        <p>this is going to be the description</p>
+      </div>
+      <p>i need to add hyperlinks for the repo and demo</p>
+    </div>
+
+
+    </div>
+    
+  )
+}
+
+export default Projects;
